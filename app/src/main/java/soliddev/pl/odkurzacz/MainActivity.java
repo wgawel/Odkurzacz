fd84d6b035857c52b3895a0405cb3b9fcf2112cb
Wojciech Gawel <wojtekgaw@gmail.com>
2020-03-26 16:07:27 +0100

    Add delayed stop function.

--- app/src/main/java/soliddev/pl/odkurzacz/MainActivity.java
+++ app/src/main/java/soliddev/pl/odkurzacz/MainActivity.java
@@ -2,6 +2,7 @@ package soliddev.pl.odkurzacz;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,17 +12,24 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private static final String BTN_STATE = "BTN_STATE";
     private Button btnStart;
     private Button btnStop;
+    private Button btnDelayedStop;
+    private Button btnDelayedStopDecrease;
+    private Button btnDelayedStopIncrease;
     private Button btnClose;
     private Button btnBatterySettings;
     private Switch switchNightMode;
     private View generalLayout;
     private TextView titleLabel;
     private TextView downloadLatestLabel;
+    private int delayedStopTimerDefaultValue = 15*60000;
+    private int delayedStopTimer = delayedStopTimerDefaultValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
+        btnDelayedStop = (Button) findViewById(R.id.btnDelayedStop);
+        btnDelayedStopDecrease = (Button) findViewById(R.id.btnDelayedStopDecrease);
+        btnDelayedStopIncrease = (Button) findViewById(R.id.btnDelayedStopIncrease);
         btnClose = (Button) findViewById(R.id.btnClose);
         btnBatterySettings = (Button) findViewById(R.id.btnBatterySettings);
         switchNightMode = (Switch) findViewById(R.id.switchNightMode);
@@ -39,12 +50,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
+        btnDelayedStop.setOnClickListener(this);
+        btnDelayedStopDecrease.setOnClickListener(this);
+        btnDelayedStopIncrease.setOnClickListener(this);
         btnClose.setOnClickListener(this);
         btnBatterySettings.setOnClickListener(this);
         switchNightMode.setOnCheckedChangeListener(this);
         btnStart.setEnabled(true);
         btnStop.setEnabled(false);
         btnClose.setEnabled(true);
+
+        delayedStopTimer = delayedStopTimerDefaultValue;
     }
 
     @Override
@@ -52,9 +68,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boolean[] btnState = savedInstanceState.getBooleanArray(BTN_STATE);
         btnStart.setEnabled(btnState[0]);
         btnStop.setEnabled(btnState[1]);
-        btnClose.setEnabled(btnState[2]);
+        btnDelayedStop.setEnabled(btnState[2]);
+        btnDelayedStopDecrease.setEnabled(btnState[3]);
+        btnDelayedStopIncrease.setEnabled(btnState[4]);
+        btnClose.setEnabled(btnState[5]);
 
-        boolean nightMode = btnState[3];
+        boolean nightMode = btnState[6];
         switchNightMode.setChecked(nightMode);
         if (nightMode) {
             switchToNightMode();
@@ -68,6 +87,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         boolean[] btnState = {
                 btnStart.isEnabled(),
                 btnStop.isEnabled(),
+                btnDelayedStop.isEnabled(),
+                btnDelayedStopDecrease.isEnabled(),
+                btnDelayedStopIncrease.isEnabled(),
                 btnClose.isEnabled(),
                 switchNightMode.isChecked()
         };
@@ -86,22 +108,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnStart.setEnabled(false);
             btnStop.setEnabled(true);
         } else if (view == btnStop) {
-            //stopping service
-            stopService(new Intent(this, MusicService.class));
-            btnStart.setEnabled(true);
-            btnStop.setEnabled(false);
+            stopAction();
         } else if (view == btnClose) {
             //stopping service
             stopService(new Intent(this, MusicService.class));
             // close app
             moveTaskToBack(true);
             finish();
+        } else if (view == btnDelayedStop) {
+            delayedStopAction();
+        } else if (view == btnDelayedStopDecrease) {
+            delayedStopTimer -= 60000;
+        } else if (view == btnDelayedStopIncrease) {
+            delayedStopTimer += 60000;
         } else if (view == btnBatterySettings) {
             startActivity(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
             Toast.makeText(this,
                     "Wyświetl wszystkie aplikacje, znajdź Odkurzacz i wyłącz optymalizację baterii.",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void stopAction() {
+        //stopping service
+        stopService(new Intent(this, MusicService.class));
+        btnStart.setEnabled(true);
+        btnStop.setEnabled(false);
+    }
+
+    private void delayedStopAction() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                delayedStopTimer -= 1000;
+
+                int delayedStopTimerInSeconds = delayedStopTimer/1000;
+
+                long minutes = TimeUnit.SECONDS.toMinutes(delayedStopTimerInSeconds);
+                long seconds = TimeUnit.SECONDS.toSeconds(delayedStopTimerInSeconds) - (TimeUnit.SECONDS.toMinutes(delayedStopTimerInSeconds) *60);
+
+                String label = "Wyłączenie za "+String.format("%02d:%02d", minutes, seconds);
+
+                btnDelayedStop.setText(label);
+
+                if ( delayedStopTimer <= 0 ) {
+                    stopAction();
+                    delayedStopTimer = delayedStopTimerDefaultValue;
+                } else {
+                    delayedStopAction();
+                }
+            }
+        }, 1000);
     }
 
     @Override
